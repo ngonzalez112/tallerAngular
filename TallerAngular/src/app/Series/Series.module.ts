399ae32b2b7c242778558249d61e9639834bd579
ngonzalez112 <n.gonzalez112@uniandes.edu.co>
2024-10-27 21:23:55 -0500

    angular inicio

---
+++ TallerAngular/src/app/Series/Series.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SeriesComponent } from './Series.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SeriesComponent]
+})
+export class SeriesModule { }
